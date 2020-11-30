81ec8ee30594174f6c3d2dc91f5c6676e97f2fc0
dastasoft <dastasoft@gmail.com>
2020-12-01 00:15:39 +0100

    finished module 3

--- 1-rn-starter/App.js
+++ 1-rn-starter/App.js
@@ -3,14 +3,16 @@ import { createStackNavigator } from "react-navigation-stack";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App",
     },
