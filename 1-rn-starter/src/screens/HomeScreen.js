64dc2bfdb2b96bceb5e64271af76224efbba26cf
dastasoft <dastasoft@gmail.com>
2020-12-02 23:27:02 +0100

    finished module 4

--- 1-rn-starter/src/screens/HomeScreen.js
+++ 1-rn-starter/src/screens/HomeScreen.js
@@ -1,25 +1,26 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button
-        onPress={() => console.log("Button pressed")}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
-      <TouchableOpacity onPress={() => console.log("List Pressed")}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo"
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
-  },
+    fontSize: 30
+  }
 });
 
 export default HomeScreen;
