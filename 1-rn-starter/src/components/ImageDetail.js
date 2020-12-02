49872c4e2b150056ec707f3d786f972fbb4f8a38
dastasoft <dastasoft@gmail.com>
2020-12-03 00:11:48 +0100

    finished module 5

---
+++ 1-rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const ImageDetail = ({ title, imageSource, score }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image score - {score}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- 1-rn-starter/src/screens/HomeScreen.js
+++ 1-rn-starter/src/screens/HomeScreen.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button
-        onPress={() => navigation.navigate('Components')}
+        onPress={() => navigation.navigate("Components")}
         title="Go to Components Demo"
       />
       <Button
-        onPress={() => navigation.navigate('List')}
+        onPress={() => navigation.navigate("List")}
         title="Go to List Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to Image Demo"
       />
     </View>
   );
