1c44b0850d8fa125004360f0605b2ba984a605e7
dastasoft <dastasoft@protonmail.com>
2021-02-05 23:54:20 +0100

    added input search

--- 2-food/src/components/SearchBar.js
+++ 2-food/src/components/SearchBar.js
@@ -1,21 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View style={styles.background}>
-      <Feather name="search" size={30} />
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather style={styles.iconStyle} name="search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 15,
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row"
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
     marginHorizontal: 15
   }
 });
--- 2-food/src/screens/SearchScreen.js
+++ 2-food/src/screens/SearchScreen.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => console.log("on term submitted")}
+      />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
